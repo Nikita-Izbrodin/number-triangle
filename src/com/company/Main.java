1e41f1a8453ea0f024d5b8ba44f910c88acdcfe7
NI210248 <NI210248@reigate.ac.uk>
2021-09-24 10:59:41 +0100

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,23 @@
 package com.company;
 
+import java.sql.SQLOutput;
+import  java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Enter a number:");
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        System.out.println(number);
 
+        String tempNum = "";
+
+        for (int i = 0; i < number; ++i) {
+            //System.out.println(i);
+            tempNum +=i;
+            System.out.println(tempNum);
+        }
     }
 }
